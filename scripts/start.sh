394592c1a0d91bc6131d393c13550fadebe3c851
rugang chen <urugang@cryptape.com>
2017-07-17 21:55:23 +0800

    fix engine_signer

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-RUST_LOG=evm=debug nohup  parity --config config/node0.toml > /tmp/node0.log 2>&1 &
-nohup parity ui --config config/node1.toml --ui-no-validation --ports-shift=100  > /tmp/node1.log 2>&1 &
+RUST_LOG=evm,state=trace nohup parity ui --config config/node0.toml --ui-no-validation                    > /tmp/node0.log 2>&1 &
+RUST_LOG=evm,state=trace nohup parity ui --config config/node1.toml --ui-no-validation --ports-shift=100  > /tmp/node1.log 2>&1 &
